3c76162724082c83a7dc756be549db611b5d2b1e
emerald-e <ilaf.jawahire01@gmail.com>
2023-06-02 23:05:53 +0530

    php sql module

---
+++ my fy project.php
@@ -0,0 +1,232 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<title>EMERALD</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+
+
+
+body {
+  font-family: "Lato", sans-serif;
+}
+
+
+.sidepanel  {
+  width: 0;
+  position: fixed;
+  z-index: 1;
+  height: 640px;
+  top: 0;
+  left: 0;
+  background-color: lightgrey;
+  overflow-x: hidden;
+  transition: 0.5s;
+  padding-top: 60px;
+}
+
+.sidepanel a {
+ 
+  padding: 8px 8px 8px 32px;
+  text-decoration: none;
+  font-size: 25px;
+  color: grey;
+  display: block;
+  transition: 0.3s;
+}
+
+.sidepanel a:hover {
+  color: white;
+}
+
+.sidepanel .closebtn {
+  position: absolute;
+  top: 0;
+  right: 25px;
+  font-size: 36px;
+}
+
+.openbtn {
+  
+  font-size: 20px;
+  cursor: pointer;
+  background-color: lightgrey;
+  color: white;
+  padding: 10px 15px;
+  border: none;
+}
+
+.openbtn:hover {
+  background-color:darkgray;
+}
+
+</style>
+ <style>
+
+ a:link, a:visited{
+	background-color:gray;
+	background-color:lightgrey;3
+	color:white;
+	padding:15px 25px;
+	
+	text-align :center;
+    display:inline-block;
+	text-decoration: none;
+
+}
+a:hover , a:active{
+	background-color:darkgray;
+}-->
+
+	</style>
+	</head>
+<body>
+ <?php 
+//echo "<hr/>";    darkgray
+//echo"<img src=\"profile1.jpg\"alt=\"profile\">";
+//?>
+<p align="right">
+
+ &nbsp <img src="https://cdn-icons-png.flaticon.com/512/5087/5087579.png" alt="Login" width="28" height="28">
+
+	  &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp <img src="https://img.icons8.com/external-flaticons-lineal-color-flat-icons/512/external-cart-100-most-used-icons-flaticons-lineal-color-flat-icons.png" alt="Shopping cart" width="28" height="28">
+	
+		&nbsp &nbsp <br><a href="http://localhost/xampp/shoppingwlogin/login.php" >Login</a>
+		&nbsp  <a href="shoppingbag.html">Bag</a> 
+               <!--   <a href="http://localhost/login/register.php/   http://localhost/shoppingwlogin/register.php">Register</a>-->
+				<br><br></p>
+
+<div align="left" id="mySidepanel" class="sidepanel">
+  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">×</a>
+  <br>
+  
+
+<!--<ul>
+<p><b>MY ORDER</b></p>-->
+     <!--<a href="shoppingbag.html">Cart</a><br>-->
+    <!--<p><b>ACCOUNTS</b></p>-->
+    <a href="http://localhost/login/login.php/">Login</a>
+    <a href="http://localhost/shoppingwlogin/register.php">Sign Up</a><br>
+	<a href="http://localhost/shoppingwlogin/index.php">My Orders</a>
+	<!--<p><b>STORES</b></p>-->
+    <!--<a href="storenearby.html">Stores</a><br>-->
+	<!--<p><b>REVIEW</b></p>-->
+	<a href="#">Home</a>
+    <a href="#">About</a>
+    <a href="#">Services</a>
+    <a href="#">Contact</a>
+    <a href="http://localhost/login/feedback.php/">Feedback</a>
+	<a href="http://localhost/login/storenearby.php/">Newsletter</a>
+	<a href="">Subscribe</a>
+    
+<!--</ul>-->
+</div>
+
+
+
+
+
+<button  class="openbtn" onclick="openNav()" >☰</button>  
+
+<script>
+function openNav() {
+  document.getElementById("mySidepanel").style.width = "225px";
+}
+
+function closeNav() {
+  document.getElementById("mySidepanel").style.width = "0";
+}
+</script>
+
+
+
+<!--<br>
+  			
+  			
+	  	<p align="right"><a href="options details.html">|||</a></p> -->	
+			
+	
+<BR>
+
+
+<?php
+//echo 
+//foreach(glob("green emerald2.png") as $filename){
+	
+//	echo "<p>$filename</p><br>";
+//	echo "<img src ='$filename' alt='$filename.png'>";}
+//?>
+   
+<?php 
+//echo "<hr/>"
+//echo"<img src=\"green emerald\"alt=\"EMERALD\">";
+//?>
+
+<!--<center><img src="https://www.nicepng.com/png/detail/220-2200815_green-seal-about-green-seal-p-nerships-emerald.png" alt="Green Seal > About Green Seal > P, Nerships - Emerald Green Logo@nicepng.com" width="190" height="155"></center> -->
+<!--<center><img src="https://as2.ftcdn.net/v2/jpg/03/02/41/05/1000_F_302410534_KZg6GWVyrwmYsUa9TXk7AudHy2gcIw0T.jpg" alt="green seal" width="200" height="200"></center> -->
+<!--<center><img src="https://www.pngitem.com/pimgs/m/531-5314520_emerald-logo-calligraphy-hd-png-download.png" alt="green" width="350" height="150"></center> -->
+<center><img src ="https://www.campussafetymagazine.com/wp-content/uploads/2020/02/Emerald_Logo_Primary-RGB_Web.jpg" alt="green" width="350" height="150"></center>
+
+<h2 style="background-color:teal;" align="center">THE BEST FASHION STATION FOR YOU</h2>
+<marquee width="100%"direction="right"height="100%" 
+marquee behaviour="scroll" direction="right"scrollamount="10">
+<img src ="https://logoeps.com/wp-content/uploads/2013/01/dolce-and-gabbana-logo-vector.png" width="200" height="100" >&nbsp &nbsp &nbsp 
+<img src ="https://1000logos.net/wp-content/uploads/2020/01/Diesel-Logo-1978.png" width="200" height="100">
+&nbsp &nbsp &nbsp 
+<img src ="https://cdn.dribbble.com/users/1161517/screenshots/7463720/fila_logo_still_2x.gif?compress=1&resize=400x300" width="200" height="100">
+&nbsp &nbsp &nbsp
+<img src="https://www.luxuryabode.com/blog/blogimages/givenchy-logo.jpg" width="200" height="100">
+&nbsp &nbsp &nbsp
+<img src="https://i.pinimg.com/564x/72/cb/f7/72cbf791d353efe37e7b4d9f053ba20e.jpg" width="200" height="100">
+&nbsp &nbsp &nbsp
+<img src="https://1000logos.net/wp-content/uploads/2020/01/Emporio-Armani-logo.jpg" width="200" height="100">
+&nbsp &nbsp &nbsp 
+<img src="https://m.media-amazon.com/images/S/abs-image-upload-na/3/AmazonStores/A21TJRUUN4KGV/04c28b0a0a586453f65f899d561c607d.w400.h400.jpg" alt="" width="200" height="100"> &nbsp &nbsp &nbsp 
+<img src="https://1000logos.net/wp-content/uploads/2021/11/Calvin-Klein-Logo-1968.png" width="200" height="100">
+&nbsp &nbsp &nbsp
+<img src ="http://www.hm.com/entrance/assets/bundle/img/HM-Share-Image.jpg" width="200" height="100">
+&nbsp &nbsp &nbsp
+<img src="https://1757140519.rsc.cdn77.org/blog/wp-content/uploads/2020/03/the-4th-logo-1024x862.jpg" width="200" height="100">
+&nbsp &nbsp &nbsp
+<img src="https://1000logos.net/wp-content/uploads/2020/06/Marks-Spencer-Logo-2007.jpg" width="200" height="100"> &nbsp &nbsp &nbsp
+<img src ="https://seeklogo.com/images/J/jack-and-jones-logo-A33E08E6BE-seeklogo.com.png" width="200" height="65">
+</marquee>
+<h2 style="background-color:aquamarine;"align="center">SHOPPER'S ONE STOP</h2>
+	 <!--AQUAMARINE;"-->
+	 
+		<div class="main" id="section2">
+		<!--"https://img.freepik.com/free-vector/atelier-flat-minimal-facebook-cover_23-2149403751.jpg?w=2000" --> 
+		<p align ="center"><img src ="https://wallpaperaccess.com/full/2489626.jpg" width= "1200" height="550"></p>
+		
+		<h2 align="center"><span style="color:skyblue;font-weight:bold">ONCE UPON A TIME IN INDIA ...</span></h2>
+	   <h2 align="center"><span style="color:yellowgreen;font-weight:bold">HI FIVE ATTIRES @ UNBELIEVABLE PRICES </span></h2> 
+	  
+	   <br>
+			<br>
+		 <h1><span style="color:lightpink;font-weight:bold">WOMENS</span></h1>
+			<!--<img src ="https://couponswala.com/blog/wp-content/uploads/2022/08/pantaloons-end-of-season-sale-696x392.jpg"   "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/fashion-sale-banner-template-design-389dc7a74f096738d1d425314404a2cd_screen.jpg?ts=1605613724"  "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/summer-fashion-sale-banner-design-template-62077c541db2b288dbccd6d9d1c9af3d_screen.jpg?ts=1649932911"  
+			"https://i.pinimg.com/736x/a1/26/e7/a126e7c4155a8d9d438fad9cd59035be.jpg"  "https://publish-p33712-e119997.adobeaemcloud.com/content/dam/adityabirlafashionandretailprogram/categorypage/nov/kids/MidSeasonSaleWomen-M.jpg.transform/i640x329/image.jpeg"			>-->
+			<img src="https://miro.medium.com/max/1400/1*dB_qRwat8PMd37UbhzYktQ.jpeg" width="800" height="">
+      <a href="http://localhost/shoppingwlogin/index.php" class="w3-button w3-black w3-padding-large w3-large" align ="right">SHOP NOW</a>
+	  
+		
+			<h1 align="right"><span style="color:lightblue;font-weight:bold">MENS</span></h1>
+			<p align="right">
+			<a href="http://localhost/shoppingwlogin/index.php" class="w3-button w3-black w3-padding-large w3-large" align ="right">SHOP NOW</a>
+			<img src ="https://www.abfrl.com/img/business/pantaloon/image004.jpg?2" width="800" height="400">
+			<!--<img src ="https://publish-p33712-e119997.adobeaemcloud.com/content/dam/adityabirlafashionandretailprogram/categorypage/nov/kids/MidSeasonSaleMen-M.jpg.transform/i640x329/image.jpeg">-->
+			</p>
+	  
+	
+
+	  
+
+<p align="right" <b><h2 style="background-color:black;" align="right"><span style="color:lightgrey;font-weight:bold">FOLLOW US ON</h2></b></span></p>
+
+<p style="background-color:black;" align="right" height="55"><br><img src="https://img.icons8.com/color/512/twitter--v4.png" height="44" width="44">
+<img src="https://img.icons8.com/color/512/facebook-new.png" height="44" width="44">
+<img src="https://img.icons8.com/fluency/512/instagram-new.png" height="44" width="44"></p>
+
+
+</body>
+</html>
